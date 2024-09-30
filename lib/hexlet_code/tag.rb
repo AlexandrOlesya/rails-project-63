138bd78218245771b7a029c72931a428c6edf0d6
Alexandr Mochalov <mochalov.alexandr@gmail.com>
2024-09-30 16:15:24 +0400

    Add class tag and test

---
+++ lib/hexlet_code/tag.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module HexletCode
+class Tag
+    def self.build(tag, attrs = {})
+      content = block_given? ? yield : nil
+      attributes = attrs.map { |k, v| "#{k}='#{v}'" }.join(' ')
+
+      if content
+        attributes.empty? ? "<#{tag}>#{content}</#{tag}>" : "<#{tag} #{attributes}>#{content}</#{tag}>"
+      else
+        "<#{tag}#{attributes.empty? ? '' : ' ' + attributes}>"
+      end
+    end
+end
+end
